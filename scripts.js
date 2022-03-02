691d8036aea079a40c14f38d2f7357c492516f54
Vera <vera.k.loui@gmail.com>
2022-03-01 16:57:06 -0800

    Website

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var name = "Vera";
+console.log(name);
+name = "Jammie"
+console.log(name)
+var number = 5
+console.log(number+6);
+/*
+console.log(5);
+console.log('He' + 'llo'); 
+console.log('Testing');
+console.log(123);
+console.log('   Hello '.length);
+console.log('1' + '' + '2');
+*/ 
